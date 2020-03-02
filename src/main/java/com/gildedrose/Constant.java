83bc882aab76a813d39b4c70b133fabb35f764e4
zdazx <zhangduo0227@163.com>
2020-03-02 16:09:12 +0800

    [zhangduo] refactor: add class to store constant

---
+++ src/main/java/com/gildedrose/Constant.java
@@ -0,0 +1,7 @@
+package com.gildedrose;
+
+public class Constant {
+    public static final String BACKSTAGE_PASSES_TO_A_TAFKAL_80_ETC_CONCERT = "Backstage passes to a TAFKAL80ETC concert";
+    public static final String AGED_BRIE = "Aged Brie";
+    public static final String SULFURAS_HAND_OF_RAGNAROS = "Sulfuras, Hand of Ragnaros";
+}
