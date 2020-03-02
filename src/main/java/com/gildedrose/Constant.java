bc87ae8d95f927a4381da80e3bc72fe805f7848e
zdazx <zhangduo0227@163.com>
2020-03-02 17:16:20 +0800

    [zhangduo] refactor: add some constant of number

--- src/main/java/com/gildedrose/Constant.java
+++ src/main/java/com/gildedrose/Constant.java
@@ -4,4 +4,7 @@ public class Constant {
     public static final String BACKSTAGE_PASSES_TO_A_TAFKAL_80_ETC_CONCERT = "Backstage passes to a TAFKAL80ETC concert";
     public static final String AGED_BRIE = "Aged Brie";
     public static final String SULFURAS_HAND_OF_RAGNAROS = "Sulfuras, Hand of Ragnaros";
+    public static final int NUM_50 = 50;
+    public static final int NUM_11 = 11;
+    public static final int NUM_6 = 6;
 }
